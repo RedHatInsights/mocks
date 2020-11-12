8d42d49c8db7afb8bccc07b1682a666a8ddeb492
Brandon Squizzato <bsquizza@redhat.com>
2020-11-12 16:37:47 -0500

    Add db.py

---
+++ crcmocks/db.py
@@ -0,0 +1,27 @@
+from tinydb import TinyDB, Query
+from tinydb.storages import MemoryStorage
+
+
+user_db = TinyDB(storage=MemoryStorage)
+request_db = TinyDB(storage=MemoryStorage)
+query = Query()
+
+
+def all_users():
+    return user_db.all()
+
+
+def add_user(data):
+    user_db.upsert(data, query.username == data["username"])
+
+
+def all_requests():
+    return request_db.all()
+
+
+def add_request(data):
+    request_db.insert(data)
+
+
+def clear_requests():
+    request_db.purge()
