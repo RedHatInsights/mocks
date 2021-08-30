3bd1a349660efa9b1c5b3ac7c24f20187e668e44
Brandon Squizzato <bsquizza@redhat.com>
2021-08-30 11:31:26 -0400

    Deploy to ephemeral env for pr_check

--- pr_check.sh
+++ pr_check.sh
@@ -1,2 +1,26 @@
-echo "todo..."
-exit 0
+export IMAGE="quay.io/cloudservices/mocks"  # the image location on quay
+
+# Install bonfire repo/initialize
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
+
+# Build the image and push to quay
+source $CICD_ROOT/build.sh
+
+# Test that the deployment works
+source ${CICD_ROOT}/_common_deploy_logic.sh
+export NAMESPACE=$(bonfire namespace reserve)
+bonfire deploy \
+    gateway insights-ephemeral \
+    --source=appsre \
+    --set-template-ref mocks=${GIT_COMMIT} \
+    --set-image-tag ${IMAGE}=${IMAGE_TAG} \
+    --namespace ${NAMESPACE} \
+
+# Create a 'dummy' junit result file so Jenkins will not fail
+mkdir -p $WORKSPACE/artifacts
+cat << EOF > $WORKSPACE/artifacts/junit-dummy.xml
+<testsuite tests="1">
+    <testcase classname="dummy" name="dummytest"/>
+</testsuite>
+EOF
